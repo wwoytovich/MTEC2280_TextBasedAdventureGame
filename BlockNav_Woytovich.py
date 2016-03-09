abcb1806b6254a15a7a8dee311d1911e1b6d9ec5
wwoytovich <woody@woytovich.com>
2016-03-09 15:37:30 -0500

    block nav
    navigate through map, very early in dev

---
+++ BlockNav_Woytovich.py
@@ -0,0 +1,5 @@
+xvalues = int(input("Dungeon rows please: "))
+yvalues = int(input("Dungeon Columns please: "))
+dungeonPOS = [[0 for x in range(yvalues)] for x in range(xvalues)] 
+print(dungeonPOS)
+
