330cb167ef72138d3e0c4030c7edad556ba10418
wwoytovich <woody@woytovich.com>
2016-03-13 19:39:49 -0400

    able to go into rooms and create map basically
    got more logic in

--- BlockNav_Woytovich.py
+++ BlockNav_Woytovich.py
@@ -3,3 +3,33 @@ yvalues = int(input("Dungeon Columns please: "))
 dungeonPOS = [[0 for x in range(yvalues)] for x in range(xvalues)] 
 print(dungeonPOS)
 
+currentDungeon = dungeonPOS
+
+def clearOldroom():
+	currentDungeon = [[0 for x in range(yvalues)] for x in range(xvalues)] 
+
+def roomNavGOD():
+	room = input("what room do you want to go into? (x,y)")
+	roomx = int(room[0])
+	roomy = int(room[2])
+	clearOldroom
+	currentDungeon[roomx][roomy] = 1
+	print("you are in the " + room[0] + "," + room[2] + "room")
+
+roomNavGOD()
+print(currentDungeon)
+
+
+
+
+
+while #the current cell does not equal the end cell	
+	find availabe
+	from current cell 
+		if right cell is free then go there
+		elif go down
+		elif go up
+		elif go left
+	add one to counter
+if counter 	ends
+	call re-random bad cells
