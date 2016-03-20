7eedcb23930b78bb188188d826e5c65dfd37484a
wwoytovich <woody@woytovich.com>
2016-03-20 15:18:59 -0400

    Grid Travel Update
    you can travel through the grid, set grid size and see current room,
    more to come

--- BlockNav_Woytovich.py
+++ BlockNav_Woytovich.py
@@ -9,6 +9,8 @@ def clearOldroom():
 	currentDungeon = [[0 for x in range(yvalues)] for x in range(xvalues)] 
 
 def roomNavGOD():
+	global roomx
+	global roomy
 	room = input("what room do you want to go into? (x,y)")
 	roomx = int(room[0])
 	roomy = int(room[2])
@@ -23,13 +25,32 @@ print(currentDungeon)
 
 
 
-while #the current cell does not equal the end cell	
-	find availabe
-	from current cell 
-		if right cell is free then go there
-		elif go down
-		elif go up
-		elif go left
-	add one to counter
-if counter 	ends
-	call re-random bad cells
+while (currentDungeon[xvalues - 1][yvalues - 1] != 1):
+	askDir = input("Where would you like to go? [North, South, East, West]")
+	print(askDir.lower())
+	if askDir == "east" and (roomx + 1) <= (xvalues - 1):
+		roomx = roomx + 1
+		clearOldroom
+		currentDungeon[roomx][roomy] = 1
+			
+	elif (((roomx - 1) > -1 ) and (askDir.lower() == "west")):
+		roomx = roomx - 1
+		clearOldroom
+		currentDungeon[roomx][roomy] = 1
+			
+	elif ((askDir.lower() == "north") and ((roomy + 1) <= (yvalues - 1))):
+		roomy = roomy + 1
+		clearOldroom
+		currentDungeon[roomx][roomy] = 1
+			
+	elif (((roomy - 1) > -1 ) and (askDir.lower() == "south")):
+		roomy = roomy - 1
+		clearOldroom
+		currentDungeon[roomx][roomy] = 1
+			
+	else:
+		print("Your input was incorrect or invalid. Please try again")
+		print("you are currently in the " + str(roomx) + "," + str(roomy) + "room") #comment out later probably
+		continue
+			
+	print("you are in the " + str(roomx) + "," + str(roomy) + "room") #comment out later probably
