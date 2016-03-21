1e5572faf97c1684b5c6ba47b37c8ff63e38d532
wwoytovich <woody@woytovich.com>
2016-03-20 22:40:56 -0400

    fix of previous mistakes
    still working on dumb issues that are an easy fix once i figure out why
    its wrong.

--- BlockNav_Woytovich.py
+++ BlockNav_Woytovich.py
@@ -41,6 +41,8 @@ def clearOldroom():
 def scary():
 	monsterX = ["Dragon", "slim", "golin", "ogre", "nekomata",  "ghost", "zombie"]
 	monsterZ = ["ghoul", "skeleton", "spider", "snake", "kobolt", "red boar"]
+	monsterXSize = len(monsterX)
+	monsterZSize = len(monsterZ)
 	currentX = random.randrange(monsterXSize)
 	currentZ = random.randrange(monsterZSize)
 	return monsterX[currentX] + "-" + monsterZ[currentZ]
@@ -65,7 +67,8 @@ def randProbRand():
 		
 #take damgage code
 def takeDam():
-	pLife = plife - randProbRand()
+	global pLife -= random.randint(20,45)
+	
 	
 #trap room
 def trapRoom():
@@ -74,43 +77,63 @@ def trapRoom():
 		if randProb(80):
 			print("Its a trap! You took damage")
 			takeDam()
-			print("You have" + str(plife) +" health")
-			if (plife <= 0)
-				break
+			print("You have" + str(global pLife) +" health")
+			if (global pLife <= 0):
+				print("you are dead. goodbye!")
+				exit()
 		else:
 			print("There was a health in the box! +20 hp")
-			plife = plife +20
-			print("You have" + str(plife) +" health")
+			global pLife += 20
+			print("You have" + str(pLife) +" health")
 	else:
 		print("You decided to not risk it....")
 			
 #monster encounter option
 def monster():
-	print("A monster steps out of the dark...")
-	print("As it grows closer you can see its a " + scary())
-	danger = input("What do you decide to do? [Run, Battle, Freeze (and hope it doenst see you)]")
-	if danger.lower() = "run":
-		if randProb(80) == 1:
-			print("You escaped")
-		else:
-			print("You took " + str(rand.randint(0,50)) + "damage")
-	elif danger.lower() == "enter battle":
-		takeDam()
-		print("poor figting mechanics let you defeat the monster")
-			if (plife <= 0)
-				print("Unfortunately you died in the process...")
+	themunsters = true
+	while themunsters 
+		print("A monster steps out of the dark...")
+		print("As it grows closer you can see its a " + scary())
+		danger = input("What do you decide to do? [Run, Battle, Freeze (and hope it doenst see you)]")
+		if danger.lower() == "run":
+			if randProb(80) == 1:
+				print("You escaped")
 				break
-		print("You have" + str(plife) +" health left over")
-	elif danger.lower() == "freeze":
-		if randProbRand() == 1:
-			print("Holy shit! it worked")
-		else:
-			print("It didnt work! You got hit")
-			plife = plife - random.randint(5,25)
-			if (plife <= 0)
+			else:
+				tmp2 =  random.randint(0,50)
+				global pLife -= tmp2
+				print("You took " + str(tmp2) + "damage while escaping")
+				if (global pLife <= 0):
+					print("Unfortunately you died in the process...")
+					print("you are dead. goodbye!")
+					exit()
+				break			
+		elif danger.lower() == "battle":
+			takeDam()
+			print("poor figting mechanics let you defeat the monster")
+			if (global pLife <= 0):
 				print("Unfortunately you died in the process...")
+				print("you are dead. goodbye!")
+				exit()
+			print("You have" + str(pLife) +" health left over")
+			break
+		elif danger.lower() == "freeze":
+			if randProbRand() == 1:
+				print("Holy shit! it worked")
 				break
-			print("You have" + str(plife) +" health left over")
+			else:
+				print("It didnt work! You got hit")
+				global pLife -= random.randint(5,25)
+				if (global pLife <= 0):
+					print("Unfortunately you died in the process...")
+					print("you are dead. goodbye!")
+					exit()
+				print("You have" + str(global pLife) +" health left over")
+				break
+		else:
+			print("What? I didnt catch that.")
+			continue
+			
 #the free roomm
 def free():
 	print("You enter a dimly lit room, you see a figure in the distance but cannot be sure that its real")
@@ -122,22 +145,21 @@ def healthUp():
 	print("Stepping back you fall on your ass and notice a needle that says \"+20 hp\" but it looks a little used... ")
 	print("Deciding that its worth it to take the risk of long term disease you inject the contents into your thigh.")
 	print("You gained some health!")
-	plife = plife + random.randint(3,20)
-	print("You have" + str(plife) +" health left over")
+	global pLife += random.randint(3,20)
+	print("You have" + str(pLife) +" health left over")
 #enocounter
 def encounter():
 	tmp = random.randint(0,99)
 	if tmp < 10: #10 percetn
 		healthUp()
-	elif tmp >= 10 and tmp < 30 #20 percent
+	elif tmp >= 10 and tmp < 30: #20 percent
 		free()
-	elif tmp >= 30 and tmp < 70 # 40 percent
+	elif tmp >= 30 and tmp < 70: # 40 percent
 		monster()
-	elif tmp >= 70 and tmp < 100 # 30 percnet
+	elif tmp >= 70 and tmp < 100: # 30 percnet
 		trapRoom()
-	else 
-		free()
-		
+	else:
+		free()	
 #initialize status of player interaction things, such as health, damage and level for begining of game
 level = 0
 pLife = 100
@@ -148,7 +170,7 @@ pLife = 100
 
 
 
-while level < 6 or pLife <= 0:
+while level < 6 or global pLife <= 0:
 
 	# this is code that allows you to go into a different room
 	while (currentDungeon[xvalues - 1][yvalues - 1] != 1):
@@ -175,35 +197,10 @@ while level < 6 or pLife <= 0:
 			print("you are currently in the " + str(roomx) + "," + str(roomy) + "room") #comment out later probably
 			continue
 		print("you are in the " + str(roomx) + "," + str(roomy) + "room") #comment out later probably
+		encounter()
 
 
 		
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	
